8326bfdd0e2502fe8b3bfb20afe2108be7957aa7
shamimjucse <shamimimtiaz9@gmail.com>
2019-04-28 21:28:03 +0600

    Project Created

---
+++ Code Spells/src/main/java/com/codespells/Code/Spells/CodeSpellsApplication.java
@@ -0,0 +1,13 @@
+package com.codespells.Code.Spells;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSpellsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSpellsApplication.class, args);
+	}
+
+}
